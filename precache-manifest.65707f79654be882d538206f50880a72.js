fbc006103d291421296552b5f9d991f650681c59
rusjah <rusjah1@gmail.com>
2018-12-18 23:40:26 +0200

    Updates

---
+++ precache-manifest.65707f79654be882d538206f50880a72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "b703276040a7f8443892",
+    "url": "/todo-list/static/js/main.b7032760.chunk.js"
+  },
+  {
+    "revision": "0ea1e2d070f916c4fba5",
+    "url": "/todo-list/static/js/1.0ea1e2d0.chunk.js"
+  },
+  {
+    "revision": "b703276040a7f8443892",
+    "url": "/todo-list/static/css/main.1650aaff.chunk.css"
+  },
+  {
+    "revision": "476fb7e5dc42e03d8e214720b7c971ae",
+    "url": "/todo-list/index.html"
+  }
+];
